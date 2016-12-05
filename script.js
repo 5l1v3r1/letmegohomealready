a32622b6aace092d67536cd8ad8a8ee705a2042a
Benjamin Minixhofer <bminixhofer@gmail.com>
2016-12-05 11:34:59 +0100

    linted

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const sektor = new Sektor('.sector', {
   arc: true,
   fillCircle: false,
   sectorColor: 'black',
-  circleColor: '#444444'
+  circleColor: '#444444',
 });
 
-const getColor = percentage => {
+const getColor = (percentage) => {
     let value = percentage /= 100;
     let hue = (value * 120).toString(10);
     return `hsl(${hue}, 70%, 50%)`;
-}
+};
 
 const calculatePercentage = () => {
   let start = timeToMinutes(document.getElementById('start').value);
@@ -19,11 +19,14 @@ const calculatePercentage = () => {
 
   if(start && end) {
     let now = new Date();
-    let minutes = now.getMinutes() + now.getHours() * 60 + now.getSeconds() / 60;
+    let minutes =
+      now.getMinutes() +
+      now.getHours() * 60+
+      now.getSeconds() / 60;
 
     let percent = Math.round((minutes - start) / (end - start) * 10000) / 100;
 
-    //clamp between 0 and 100
+    // clamp between 0 and 100
     percent = Math.min(Math.max(percent, 0), 100);
     sektor.animateTo(percent * 3.6, 500);
     sektor.changeColor(getColor(percent));
@@ -31,17 +34,17 @@ const calculatePercentage = () => {
   }
 };
 
-const timeToMinutes = time => {
+const timeToMinutes = (time) => {
   let split = time.split(':').map(Number);
   return split[0] * 60 + split[1];
 };
 
-document.querySelectorAll('input').forEach(input => {
+document.querySelectorAll('input').forEach((input) => {
   let stored = localStorage[input.id];
   if(stored) {
     input.value = stored;
   }
-  input.addEventListener('change', e => {
+  input.addEventListener('change', (e) => {
     localStorage.setItem(e.target.id, e.target.value);
     calculatePercentage();
   });
